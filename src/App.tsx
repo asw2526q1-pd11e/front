1c675e5000a213330c58430fc2624ec966dcb0ca
PathologicalMiss <toni.gratacos.miralles@gmail.com>
2025-12-19 00:21:27 +0100

    App.tsx ignore

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import LoginPage from "./pages/LoginPage";
+// Aquest fitxer ja no és necessari perquè main.tsx gestiona tot el routing
+// Pots eliminar aquest fitxer o deixar-lo buit
 
 function App() {
-  return (
-    <div className="min-h-screen bg-roseTheme-soft">
-      <LoginPage />
-    </div>
-  );
+  return null;
 }
 
 export default App;
